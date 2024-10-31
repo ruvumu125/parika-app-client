793e9c1b2a2f75426d3f14864b7df5c9770c8529
koda <kodaenterprisesolution@gmail.com>
2024-10-31 12:38:13 +0200

    First commit

--- src/pages/Vehicule.js
+++ src/pages/Vehicule.js
@@ -228,124 +228,124 @@ const VehiculeType = () => {
 
             <div className="card table-list-card">
                 <div className="card-body">
-
-                    {/*skeleton*/}
-                    {isLoading && <ListSkleton title="Liste de véhicules"/>}
-                    {/*skeleton*/}
-
-                    {/*list section*/}
                     {
-                        isDataFound && (<><div className="table-top">
-                            <div className="search-set">
-                                <h4>Liste de véhicules</h4>
+                        isLoading ? (<ListSkleton title="Liste de véhicules"/>):(
+                            <>
+                                <div className="table-top">
+                                    <div className="search-set">
+                                        <h4>Liste de véhicules</h4>
 
-                            </div>
-                            <div className="search-path">
-                                <input
-                                    value={searchTerm}
-                                    onChange={handleSearchChange}
-                                    placeholder="Rechercher type de vehicule"
-                                    type="text" className="form-control"/>
-                            </div>
+                                    </div>
+                                    <div className="search-path">
+                                        <input
+                                            value={searchTerm}
+                                            onChange={handleSearchChange}
+                                            placeholder="Rechercher type de vehicule"
+                                            type="text" className="form-control"/>
+                                    </div>
 
-                        </div>
-                            <div className="table-responsive">
-                                <table className="table">
-                                    <thead>
-                                    <tr>
-                                        <th>Nº d'immatriculation</th>
-                                        <th>Type de vehicule</th>
-                                        <th>Numéro de compte</th>
-                                        <th>Solde du compte</th>
-                                        <th>Date de création</th>
-                                        <th className="no-sort">Action</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {
-                                        data.map((vehicule_type) => {
+                                </div>
+                                {
+                                    data.length===0 ?(<NoDataFound name={name}/>):(
+                                        <>
+                                            <div className="table-responsive">
+                                                <table className="table">
+                                                    <thead>
+                                                    <tr>
+                                                        <th>Nº d'immatriculation</th>
+                                                        <th>Type de vehicule</th>
+                                                        <th>Numéro de compte</th>
+                                                        <th>Solde du compte</th>
+                                                        <th>Date de création</th>
+                                                        <th className="no-sort">Action</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    {
+                                                        data.map((vehicule_type) => {
 
-                                            const date=moment(vehicule_type.creationDate).format('LLLL');
-                                            return(
-                                                <tr key={vehicule_type.idVehicle}>
-                                                    <td>{vehicule_type.registrationNumber}</td>
-                                                    <td>{vehicule_type.vehicleType.vehiculeTypeName}</td>
-                                                    <td>{vehicule_type.accountNumber}</td>
-                                                    <td>{vehicule_type.solde}</td>
-                                                    <td>{date}</td>
-                                                    <td className="text-center">
-                                                        <a className="action-set" href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
-                                                            <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-                                                        </a>
-                                                        <ul className="dropdown-menu">
-                                                            <li>
-                                                                <a
-                                                                    onClick={(e) =>  openAddEditModal(2,vehicule_type.id,vehicule_type.registrationNumber,vehicule_type.vehicleType.vehiculeTypeName) }
-                                                                    className="dropdown-item"
-                                                                    data-bs-toggle="modal"
-                                                                    data-bs-target="#sales-details-new">
-                                                                    <svg
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        width="24"
-                                                                        height="24"
-                                                                        viewBox="0 0 24 24"
-                                                                        fill="none"
-                                                                        stroke="currentColor"
-                                                                        strokeWidth="2"
-                                                                        strokeLinecap="round"
-                                                                        strokeLinejoin="round"
-                                                                        className="feather feather-edit info-img">
-                                                                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                                                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                                                    </svg>Modifier le véhicule
-                                                                </a>
-                                                            </li>
+                                                            const date=moment(vehicule_type.creationDate).format('LLLL');
+                                                            return(
+                                                                <tr key={vehicule_type.idVehicle}>
+                                                                    <td>{vehicule_type.registrationNumber}</td>
+                                                                    <td>{vehicule_type.vehicleType.vehiculeTypeName}</td>
+                                                                    <td>{vehicule_type.accountNumber}</td>
+                                                                    <td>{vehicule_type.solde}</td>
+                                                                    <td>{date}</td>
+                                                                    <td className="text-center">
+                                                                        <a className="action-set" href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
+                                                                            <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                                                        </a>
+                                                                        <ul className="dropdown-menu">
+                                                                            <li>
+                                                                                <a
+                                                                                    onClick={(e) =>  openAddEditModal(2,vehicule_type.id,vehicule_type.registrationNumber,vehicule_type.vehicleType.vehiculeTypeName) }
+                                                                                    className="dropdown-item"
+                                                                                    data-bs-toggle="modal"
+                                                                                    data-bs-target="#sales-details-new">
+                                                                                    <svg
+                                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                                        width="24"
+                                                                                        height="24"
+                                                                                        viewBox="0 0 24 24"
+                                                                                        fill="none"
+                                                                                        stroke="currentColor"
+                                                                                        strokeWidth="2"
+                                                                                        strokeLinecap="round"
+                                                                                        strokeLinejoin="round"
+                                                                                        className="feather feather-edit info-img">
+                                                                                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                                                                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                                                                    </svg>Modifier le véhicule
+                                                                                </a>
+                                                                            </li>
 
-                                                            <li>
-                                                                <a
-                                                                    onClick={() =>
-                                                                        openVehiculeDetails(vehicule_type)
-                                                                    }
-                                                                    className="dropdown-item">
-                                                                    <svg
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        width="24"
-                                                                        height="24"
-                                                                        viewBox="0 0 24 24"
-                                                                        fill="none"
-                                                                        stroke="currentColor"
-                                                                        strokeWidth="2"
-                                                                        strokeLinecap="round"
-                                                                        strokeLinejoin="round"
-                                                                        className="feather feather-download info-img">
-                                                                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                                                                        <polyline points="7 10 12 15 17 10"></polyline>
-                                                                        <line x1="12" y1="15" x2="12" y2="3"></line>
-                                                                    </svg>Imprimer la carte
-                                                                </a>
-                                                            </li>
-                                                        </ul>
-                                                    </td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
+                                                                            <li>
+                                                                                <a
+                                                                                    onClick={() =>
+                                                                                        openVehiculeDetails(vehicule_type)
+                                                                                    }
+                                                                                    className="dropdown-item">
+                                                                                    <svg
+                                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                                        width="24"
+                                                                                        height="24"
+                                                                                        viewBox="0 0 24 24"
+                                                                                        fill="none"
+                                                                                        stroke="currentColor"
+                                                                                        strokeWidth="2"
+                                                                                        strokeLinecap="round"
+                                                                                        strokeLinejoin="round"
+                                                                                        className="feather feather-download info-img">
+                                                                                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                                                                        <polyline points="7 10 12 15 17 10"></polyline>
+                                                                                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                                                                                    </svg>Imprimer la carte
+                                                                                </a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </td>
+                                                                </tr>
+                                                            )
+                                                        })
+                                                    }
 
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div className="all_tables_pagination">
-                                <Pagination
-                                    totalPages={totalPages}
-                                    currentPage={page}
-                                    onPageChange={handlePageChange}
-                                />
-                            </div></>)
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <div className="all_tables_pagination">
+                                                <Pagination
+                                                    totalPages={totalPages}
+                                                    currentPage={page}
+                                                    onPageChange={handlePageChange}
+                                                />
+                                            </div>
+                                        </>
+                                    )
+                                }
+                            </>
+                        )
                     }
-
-                    {/*list section*/}
-
-
                 </div>
             </div>
 
